cc4727df8b4e934d1ae95e48611a2345922e090c
Timmy Wong <thcathy@gmail.com>
2018-06-04 17:11:21 +0800

    update member api

---
+++ src/main/java/com/esl/entity/rest/UpdateMemberRequest.java
@@ -0,0 +1,12 @@
+package com.esl.entity.rest;
+
+import java.util.Date;
+
+public class UpdateMemberRequest {
+    public String lastName;
+    public String firstName;
+    public Date birthday;
+    public String address;
+    public String phoneNumber;
+    public String school;
+}
