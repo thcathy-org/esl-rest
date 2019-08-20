8688acb06f0e87e9423a1639c920acfe8f886f5d
Timmy Wong <thcathy@gmail.com>
2019-08-20 23:56:09 +0800

    fix mysql issue

--- src/main/java/com/esl/model/Member.java
+++ src/main/java/com/esl/model/Member.java
@@ -1,32 +1,13 @@
 package com.esl.model;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Transient;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
+import com.esl.model.group.MemberGroup;
 import org.apache.commons.lang3.StringUtils;
 
-import com.esl.model.group.MemberGroup;
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.*;
 
 @Entity
 @Table(name = "member")
@@ -74,8 +55,9 @@ public class Member implements Serializable {
 	@Column(name = "EMAIL_ADDRESS")
 	private String emailAddress;
 
-	@ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
-	@JoinColumn(name = "GRADE_ID")
+	//@ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
+	//@JoinColumn(name = "GRADE_ID")
+	@Transient
 	private Grade grade;
 
 	@Column(name = "LOGINED_SESSION_ID")
